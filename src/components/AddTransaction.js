7e5a7a89012db93d1a9c85a6e94c87a979966e9f
benjhi <renzo_soler@hotmail.com>
2021-05-03 17:57:10 -0300

    master

---
+++ src/components/AddTransaction.js
@@ -0,0 +1,41 @@
+import React, { useState, useContext } from 'react';
+import { GlobalContext } from '../context/GlobalState';
+
+export const AddTransaction = () => {
+const [text, setText] = useState('');
+const [amount, setAmount] = useState(0);
+
+const { addTransaction } = useContext(GlobalContext);
+
+const onSubmit = e => {
+    e.preventDefault();
+
+    const newTransaction = {
+        id: Math.floor(Math.random() * 100000000),
+        text,
+        amount: +amount
+    }
+
+    addTransaction(newTransaction);
+}
+
+    return (
+        <>
+            <h3>Agregar nueva transaccion</h3>
+                <form onSubmit={onSubmit}>
+                     <div className="form-control">
+                        <label htmlFor="text">Texto</label>
+                        <input type="text" value={text} onChange={(e) => setText(e.target.value)} placeholder="Ingresar texto..." />
+                     </div>
+                    <div className="form-control">
+                     <label htmlFor="amount"
+                         >Cantidad <br />
+                         (Negativo - Gasto, Positivo - Ingreso)</label
+                    >
+                    <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)} placeholder="Ingresar cantidad..." />
+                    </div>
+                     <button className="btn">Agregar transaccion</button>
+                 </form>
+        </>
+    )
+}
